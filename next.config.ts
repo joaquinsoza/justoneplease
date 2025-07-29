870acc219d6325def02e57364d76fb9d0e88900b
coderipper <hello@joaquinsoza.dev>
2025-07-28 22:33:48 -0300

    build errors

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('_http_common');
-    }
-    return config;
-  },
-  // Exclude prisma directory from build
-  serverComponentsExternalPackages: ['@prisma/client', 'prisma']
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
+  }
 };
 
 export default nextConfig;
